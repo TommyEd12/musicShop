74f2a10b4585d24e0847277ba4d0df6b8ebacc6c
egorb <egorbobylev398@gmail.com>
2024-10-05 23:41:27 +0500

    NavBarStyled

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,47 +1,50 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import storeLogo from "../assets/storeLogo.jpg";
+import CatalogImage from "../assets/CatalogImage.svg";
+import ShoppingCart from "../assets/shopping-cart.svg";
+import SearchImage from "../assets/search.svg";
+import "../styles/NavBar.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" fixed="top" className="navBarContainer">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <img className="navBarLogo" src={storeLogo}></img>
+        <Navbar.Brand href="#" className="NavBarTitle">
+          <h1>MUS&CO</h1>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
+            className="d-flex flex-row NavBarContent"
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="#action1">Главная страница</Nav.Link>
+            <Nav.Link href="#action2">Каталог</Nav.Link>
+            <img className="CatalogImage" src={CatalogImage}></img>
+            <Nav.Link href="#action3">Личный кабинет</Nav.Link>
+            <Nav.Link href="#">Корзина</Nav.Link>
+            <img className="ShoppingCartImage" src={ShoppingCart}></img>
           </Nav>
-          <Form className="d-flex">
+          <Form className="Search">
             <Form.Control
               type="search"
-              placeholder="Search"
-              className="me-2"
+              placeholder="Поиск"
+              className="me-2 SearchLine"
               aria-label="Search"
+              src={SearchImage}
             />
-            <Button variant="outline-success">Search</Button>
+
+            <Button className="SearchButton">
+              <img src={SearchImage} className="SearchImage"></img>
+            </Button>
           </Form>
         </Navbar.Collapse>
       </Container>
