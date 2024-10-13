4f570587701ecad1c58af716519a675494c0cbca
egorb <egorbobylev398@gmail.com>
2024-10-14 00:43:49 +0500

    NavBarUpdated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,12 +9,14 @@ import storeLogo from "../assets/storeLogo.jpg";
 import CatalogImage from "../assets/CatalogImage.svg";
 import ShoppingCart from "../assets/shopping-cart.svg";
 import SearchImage from "../assets/search.svg";
+import UserImage from "../assets/user.svg";
+import HomeImage from "../assets/home.svg";
 import "../styles/NavBar.css";
 
 const NavBar = () => {
   return (
     <Navbar expand="lg" fixed="top" className="navBarContainer">
-      <Container fluid>
+      <Container  className="NavContent">
         <img className="navBarLogo" src={storeLogo}></img>
         <Navbar.Brand href="#" className="NavBarTitle">
           <h1>MUS&CO</h1>
@@ -27,9 +29,11 @@ const NavBar = () => {
             navbarScroll
           >
             <Nav.Link href="#action1">Главная страница</Nav.Link>
+            <img className="HomeImage" src={HomeImage}></img>
             <Nav.Link href="#action2">Каталог</Nav.Link>
             <img className="CatalogImage" src={CatalogImage}></img>
             <Nav.Link href="#action3">Личный кабинет</Nav.Link>
+            <img className="UserImage" src={UserImage}></img>
             <Nav.Link href="#">Корзина</Nav.Link>
             <img className="ShoppingCartImage" src={ShoppingCart}></img>
           </Nav>
--- src/components/ShopSlider.tsx
+++ src/components/ShopSlider.tsx
@@ -8,20 +8,20 @@ import ibanez from "../assets/ibanez.jpg";
 const ShopSlider = () => {
   return (
     <Carousel className="Shopslider" data-bs-theme="light">
-      <Carousel.Item>
-        <img className="d-block" src={fender} alt="First slide" />
+      <Carousel.Item className="Slide">
+        <img className="d-block w-100" src={fender} alt="First slide" />
         <Carousel.Caption className="Caption">
           <h5 id="CaptionText">Встречайте гитары fender</h5>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block" src={gibson} alt="Second slide" />
+      <Carousel.Item className="Slide">
+        <img className="d-block w-100" src={gibson} alt="Second slide" />
         <Carousel.Caption className="Caption">
           <h5 id="CaptionText">Second slide label</h5>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block" src={ibanez} alt="Third slide" />
+      <Carousel.Item className="Slide">
+        <img className="d-block w-100" src={ibanez} alt="Third slide" />
         <Carousel.Caption className="Caption">
           <h5 id="CaptionText">Third slide label</h5>
         </Carousel.Caption>
