1dad526f15fc62a8a50fcfa089aca244f3aadcdd
egorb <egorbobylev398@gmail.com>
2024-11-20 01:58:39 +0500

    init

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,5 +1,17 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { usersTable } from "./schema";
 
 const db = drizzle("postgres://postgres:postgres@localhost:5432/postgres");
 
+async function Main() {
+  await db.insert(usersTable).values({
+    email: "@mail.com",
+    password: "123",
+  });
+
+  const users = await db.select().from(usersTable);
+  console.log(users);
+}
+
+Main();
